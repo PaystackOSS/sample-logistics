5dd07d7b7e5ea340949d59e3dbf2b7e899fa8168
Damilola Odujoko <damilola.odujoko@paystack.com>
2023-04-19 10:14:16 +0100

    Chore: CORS clean up

--- functions/create-invoice.js
+++ functions/create-invoice.js
@@ -36,7 +36,7 @@ exports.handler = async function(event, context) {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Netlify serveless setting to allow CORS
+      'Access-Control-Allow-Origin': '*', // Netlify serveless setting to allow CORSS
       'Access-Control-Allow-Headers': 'Content-Type' // Netlify serveless setting to allow CORS
     },
     body: JSON.stringify(body),
--- functions/webhooks.js
+++ functions/webhooks.js
@@ -3,11 +3,12 @@ const { BASE_URL, SECRET_KEY } = process.env
 
 exports.handler = async function(event, context) {
 
+  // basic webhook implementation specifically for this tutorial 
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Netlify serveless setting to allow CORS
-      'Access-Control-Allow-Headers': 'Content-Type' // Netlify serveless setting to allow CORS
+      "Access-Control-Allow-Origin": "*", // Netlify serverless setting to allow CORS
+      "Access-Control-Allow-Headers": "Content-Type" // Netlify serverless setting to allow CORS
     }
   }
 }
